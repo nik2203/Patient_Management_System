f27be29adacc79cb9a7d69e029e09d6e527588c3
nik2203 <62793095+nik2203@users.noreply.github.com>
2021-12-16 19:01:25 +0530

    Updates
    Added functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import urllib, tkinter, time, random
+import urllib, tkinter, time, random, csv
 
 def take_url(n,urllist):
     for i in range(n):
@@ -8,26 +8,38 @@ def take_url(n,urllist):
             print('URL successfully added to the list')
         else:
             print('The URL',x,'is an invalid URL and was not added')
-    return x
+    return
 
 def verify_url(url):
-    if url.startswith('https://www.youtube.com/'):
+    if url.startswith('https://www.youtube.com/') or url.startswith('http://youtu.be/'):
         return True
     else:
         return False
 
 '''
-def del_url()
-
-def load_url()
-
-def categ()
-
-def pick_categ()
+def del_url(url):
 '''
+
+def load_url(*args):
+    for i in args:
+        if i.startswith('https://www.youtube.com/') or i.startswith('http://youtu.be/'):
+            url=i
+            return url
+        elif i in title_list:
+            title=i
+            return title
+        else:
+            print('Invalid input')
+            return
+
+def pick_categ(categ):
+    with open(categ,'r') as categ_file:
+        lurl=csv.reader(categ_file)
+
 
 def rand_url(fh):
     urls=fh.readlines()
     for i in urls:
         i=i.strip('\n')
     alarm_tune=random.choice(urls)
+
